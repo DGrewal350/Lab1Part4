439c47bd010ef5616ff02654cc8e6dfbc95505d2
Daniel Grewal <daniel.grewal090@myci.csuci.edu>
2015-01-28 19:47:08 -0800

    Third commit

---
+++ CarpetDriver.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CostAnalyzer;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daniel.grewal090
+ */
+public class CarpetDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double width, length;
+        
+        System.out.print("Please enter the dimension of the room (length width)");
+        length = in.nextDouble();
+        width = in.nextDouble();
+        
+        RoomDimension myRoom = new RoomDimension(length, width);
+        RoomCarpet carpet = new RoomCarpet(myRoom);
+        
+        System.out.print("The total cost to carpet this room is: $" + carpet.getTotalCost());
+        
+    }
+    
+}
--- RoomCarpet.java
+++ RoomCarpet.java
@@ -13,6 +13,8 @@ public class RoomCarpet {
     RoomDimension size;
     double carpetCost;
     
+    // Default constructor sets carpetCost to 8 per yard
+    public RoomCarpet (RoomDimension dim) { this.size = dim; this.carpetCost = 8.00; }
     public RoomCarpet (RoomDimension dim, double cost)
     {
         this.size = dim;
